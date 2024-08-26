0db89cee4e5328dd7feff001c62deb98e4011348
Daniel Oliveira <dantoliveira@icloud.com>
2024-08-27 00:48:05 +0100

    Adição de checkbox, adição de um gráfico de barras, e descrição do projeto no README.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Sprint5
+
+Web aplicativo criado para estudar ferramentas de desenvolvimento de software, tais como:
+
+• VS Code (para programar em python)
+• GIT e GIThub (para trabalhar com versões)
+• Terminal (para instalar bibliotecas de python, cloar repositorio e poder trablhar na maquina local)
+•Render (para postar o aplicativo na web)
+
+# descriçåo do aplicativo
+
+O aplicativo consiste em uma "check-box" que, de acordo com a opção selecionada, pode exibir um gráfico de barras, um histograma ou um gráfico de dispersão, com informações provenientes de um banco de dados de vendas de veículos nos Estados Unidos.
+Os gráficos exibem informações como preço, condição do veículo e quilometragem do odômetro, assim como a quantidade de vendas.
+
 link do meu webapp: https://sprint5-dkxb.onrender.com
--- app.py
+++ app.py
@@ -6,24 +6,34 @@ car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 
 st.header('Dashboard de Veículos')
 
-hist_button = st.button('Criar histograma') # criar um botão
-if hist_button: # se o botão for clicado
+
+if st.checkbox('Criar histograma'):
     # escrever uma mensagem
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer", title='Quantidade de veículos vendidos acordo com a Quilometragem do Veículo')
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
-disp_button = st.button('Criar gráfico de dispersão') # criar um botão
-if disp_button: # se o botão for clicado
+if st.checkbox('Criar gráfico de dispersão'):
     # escrever uma mensagem
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
-    # criar um histograma
-    fig = px.scatter(car_data, x="odometer", y="price")
+    # criar um gráfico de dispersão
+    fig = px.scatter(car_data, x="odometer", y="price", title='Preço de acordo com a Quilometragem do Veículo')
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+    
+if st.checkbox('Criar gráfico de barras'):
+
+     # escrever uma mensagem
+    st.write('Criando um gráfico de barras para o conjunto de dados de anúncios de vendas de carros ')
+
+    # Criar um gráfico de barras
+    fig = px.bar(car_data, x='condition', y='price', title='Preço Médio por Condição do Veículo')
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
